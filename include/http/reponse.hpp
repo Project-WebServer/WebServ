c46f828d5fca8a4fd4eb562003357a826658b772
YuRuM0 <yuleum03@gmail.com>
2026-03-31 14:07:42 +0200

    getters added

--- include/http/reponse.hpp
+++ include/http/reponse.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   reponse.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yulpark <yulpark@student.42.fr>            +#+  +:+       +#+        */
+/*   By: yulpark <yulpark@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/19 17:48:05 by yulpark           #+#    #+#             */
-/*   Updated: 2026/03/20 15:49:25 by yulpark          ###   ########.fr       */
+/*   Updated: 2026/03/27 11:29:59 by yulpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,25 @@
 #include <sstream>
 
 #include "../handlers/Response.hpp"
+#include "request.hpp"
 
+class HTTPresponse
+{
+	public:
+
+	private:
+		// status line
+		ProtocolV	_protocol_v;
+		int			_statusCode;
+		std::string	_statusPhrase;
+
+		//headers
+		std::string	_httpHeaders; // requires separation?
+
+		//msg body
+		std::string	_msgBody;
+		// exception for msg body is when the server responds to the HEAD method but not part of the methods..
+		
+		std::string response;
+		
+};
--- srcs/http/response/getters.cpp
+++ srcs/http/response/getters.cpp
@@ -1,30 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   serialise.cpp                                      :+:      :+:    :+:   */
+/*   getters.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yulpark <yulpark@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/19 17:48:12 by yulpark           #+#    #+#             */
-/*   Updated: 2026/03/27 11:32:10 by yulpark          ###   ########.fr       */
+/*   Created: 2026/03/27 11:06:05 by yulpark           #+#    #+#             */
+/*   Updated: 2026/03/27 11:06:22 by yulpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/http/response.hpp"
 
-// input: get response
-// check if has status line, header, empty line, body all together
-// put the string into buffer
-
-HTTPresponse::HTTPresponse() : _protocolv(ProtocolV::ERR), _statusPhrase(""), _httpHeaders(""),  _msgbody(""), _response("")
-{
-}
-
-HTTPrequests::~HTTPrequests()
-{
-}
-
-std::string serialise(HTTPresponse response)
-{
-	
-}
